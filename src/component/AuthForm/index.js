0805daa98d150f1a7677a3bcdc04fae891c5e608
Anastasiia Kolieva <63116890+anastasiia-kolieva@users.noreply.github.com>
2021-04-03 21:23:13 +0300

    Feature/auth page (#5)
    * added info to AuthPage and AuthForm
    
    added files AuthPage.module.css and AuthForm.module.css. Also added some first inofrmation to index.js(AuthPage and AuthForm)
    
    * fix .env
    
    * added corrections to AuthPage and AuthForm
    
    Co-authored-by: Половинка Иван <62117182+transformator98@users.noreply.github.com>

--- src/component/AuthForm/index.js
+++ src/component/AuthForm/index.js
@@ -1,1 +1,13 @@
-export { default } from './AuthForm';
+import styles from './AuthForm.module.css';
+
+export default function AuthForm() {
+  return (
+    <form>
+      <h2>You can use your Google Account to authorize:</h2>
+      <button>Google</button>
+      <h2>Or login to our app using e-mail and password</h2>
+      <input type="email" name="email" placeholder="E-mail" />
+      <input type="password" name="password" placeholder="Password" />
+    </form>
+  );
+}
--- src/views/AuthPageView.js
+++ src/views/AuthPageView.js
@@ -1,5 +1,22 @@
+
+import AuthForm from '../component/AuthForm/index';
+
 const AuthPageView = () => {
-  return <h1>Auth Page</h1>;
+  return (
+    <>
+      <div>
+        <p>
+          [ We will help you find weak points in knowledge so that you can
+          strengthen it. We will show you what is relevant to know for a QA
+          Engineer and will try to make the learning process more diverse_ ]
+        </p>
+      </div>
+      <div>
+        <AuthForm />
+      </div>
+    </>
+  );
+
 };
 
 export default AuthPageView;
