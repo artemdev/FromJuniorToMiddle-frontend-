5eb236ae0e4a8bd83fef49920d7605192acd7fcb
Anastasiia Kolieva <63116890+anastasiia-kolieva@users.noreply.github.com>
2021-04-04 02:36:58 +0300

    Feature/auth page (#6)
    * added styles for AuthForm
    
    * added styles to AuthPageView
    
    * added styles for AuthForm
    
    Co-authored-by: Половинка Иван <62117182+transformator98@users.noreply.github.com>

--- src/component/AuthForm/index.js
+++ src/component/AuthForm/index.js
@@ -2,12 +2,20 @@ import styles from './AuthForm.module.css';
 
 export default function AuthForm() {
   return (
-    <form>
-      <h2>You can use your Google Account to authorize:</h2>
-      <button>Google</button>
-      <h2>Or login to our app using e-mail and password</h2>
-      <input type="email" name="email" placeholder="E-mail" />
-      <input type="password" name="password" placeholder="Password" />
+    <form className={styles.form}>
+      <h2 className={styles.text}>
+        You can use your Google Account to authorize:
+      </h2>
+      <button className={button-google.text}>Google</button>
+      <h2 className={styles.text}>
+        Or login to our app using e-mail and password
+      </h2>
+      <input type="email" name="email" placeholder="E-mail" className={styles.input}/>
+      <input type="password" name="password" placeholder="Password" className={styles.input}/>
+      <div>
+      <button className={styles.button-signIn}>Sign in</button>
+      <button className={styles.button-signUp}>Sign up</button>
+      </div>
     </form>
   );
 }
