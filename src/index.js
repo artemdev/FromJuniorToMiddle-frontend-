dfc95bfa7db024873cf35051270f16e15e7a31ba
Половинка Иван <62117182+transformator98@users.noreply.github.com>
2021-04-05 21:04:35 +0300

    fix store

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 
 import App from './App';
 
-
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-
 // import { PersistGate } from 'redux-persist/integration/react';
 
-// import { store } from './redux/store';
+import { store } from './redux/store';
 
 // import { store, persistor } from './redux/store';
 // import Loader from 'component/Loader';
@@ -22,11 +20,11 @@ console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-      {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+    {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
     {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root'),
