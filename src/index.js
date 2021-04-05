65a2abf5a85845a0993ac5c227c9933d72522d7a
Anastasiia Kolieva <63116890+anastasiia-kolieva@users.noreply.github.com>
2021-04-05 20:00:14 +0300

    Feature/auth page (#12)
    * added styles for tablet
    
    * added styles for desktop for AuthForm
    
    Co-authored-by: Половинка Иван <62117182+transformator98@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 // import { BrowserRouter } from 'react-router-dom';
 // import { PersistGate } from 'redux-persist/integration/react';
-import store from './redux/store';
+
+// import { store } from './redux/store';
+
 // import { store, persistor } from './redux/store';
 // import Loader from 'component/Loader';
 
@@ -15,13 +17,13 @@ console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
       {/* <BrowserRouter> */}
       <App />
       {/* </BrowserRouter> */}
       {/* </PersistGate> */}
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/views/AuthPageView/AuthPageView.js
+++ src/views/AuthPageView/AuthPageView.js
@@ -8,7 +8,7 @@ const AuthPageView = () => {
         <h2 className={styles.title}>Pro Test</h2>
         <p className={styles.text}>
           <span className={styles.textBold}>[</span> We will help you find weak
-          points in knowledge so that you can strengthen it. We will show you
+          points<br /> in knowledge so that you can strengthen it. We will show you
           what is relevant to know for a{' '}
           <span className={styles.textBold}>QA Engineer</span> and will try to
           make the learning process more diverse_ <span className={styles.textBold}>]</span>
