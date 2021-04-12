45f851b58a20140e813adfc13044d7ce15731b85
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-04-12 22:26:37 +0300

    Useful info styles (#47)
    * Deletes extra files
    
    * Adds styles with images
    
    * Adds styles

--- src/views/UsefulInfo/UsefulInfo.js
+++ src/views/UsefulInfo/UsefulInfo.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import UsefulInfoItem from './UsefulInfoItem/UsefulInfoItem';
+import s from './UsefulInfo.module.scss';
 
 function UsefulInfo({ literature, resources }) {
   return (
-    <>
-      <h2>Usefull literature</h2>
-      <ol>
-        {literature.map(({ id, name, url }) => (
-          <li key={id}>
-            <UsefulInfoItem id={id} name={name} url={url} />
-          </li>
-        ))}
-      </ol>
-      <h2>Usefull resources</h2>
-      <ol>
-        {resources.map(({ id, name, url }) => (
-          <li key={id}>
-            <UsefulInfoItem id={id} name={name} url={url} />
-          </li>
-        ))}
-      </ol>
-    </>
+    <section className={s.materialsSection}>
+      <div className={s.usefullContainer}>
+        <h2 className={s.title}>Usefull literature</h2>
+        <ol className={s.list}>
+          {literature.map(({ id, name, url }) => (
+            <li className={s.item} key={id}>
+              <UsefulInfoItem id={id} name={name} url={url} />
+            </li>
+          ))}
+        </ol>
+        <h2 className={s.title}>Usefull resources</h2>
+        <ol className={s.list}>
+          {resources.map(({ id, name, url }) => (
+            <li className={s.item} key={id}>
+              <UsefulInfoItem id={id} name={name} url={url} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </section>
   );
 }
 export default UsefulInfo;
--- src/views/UsefulInfo/UsefulInfoItem/UsefulInfoItem.js
+++ src/views/UsefulInfo/UsefulInfoItem/UsefulInfoItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
-function UsefulInfoItem({ name, url }) {
+import s from '../UsefulInfo.module.scss';
+function UsefulInfoItem({ name, url, id }) {
   return (
-    <>
-      <a href={url} target="blank">
+    <span className={s.linkId}>
+      {`${id}. `}
+      <a className={s.itemLink} href={url} target="blank">
         {name}
       </a>
-    </>
+    </span>
   );
 }
 export default UsefulInfoItem;
