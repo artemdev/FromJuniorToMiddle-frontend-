4c403dd6f8568a25615825fc26e0d38d26fda21f
Ihor Nadieien <63648254+ingvar1802@users.noreply.github.com>
2021-04-13 20:12:52 +0300

    add some fixes (#56)

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,20 +1,27 @@
+import { Link } from 'react-router-dom';
+
 import './Footer.scss';
 
-import icon from '../../icon/sprite.svg';
+import iconCopyright from '../../icon/copyright.svg';
+
+import iconHeart from '../../icon/heart-footer.svg';
 
 
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer-container"> 
-
-                © 2021 | All Rights Reserved |
-                Developed with
-                <svg className="heartIcon" width="16" height="15">
-                    <use href={icon + '#heart'}></use>
-                 </svg>
-                by GoIT Students
-
+                <svg className="copyright" width="15" height="15">
+                    <use href={iconCopyright + '#Vector'}></use>
+                </svg>&nbsp;
+                2021 | All Rights Reserved | Developed with 
+                <svg className="heartIcon" width="16" height="16">
+                    <use href={iconHeart + '#Vector'}></use>
+                </svg>
+                <p>by&nbsp;</p>
+                <Link to="/contacts" className="footer-link">
+                GoIT Students
+                </Link>
             </div>
         </footer>
     )
